462f38a2002a0cdb939d02cce0972a7970672c62
Karamjot Singh <KaramjotSinghMalik@users.noreply.github.com>
2019-10-25 14:15:18 +0800

    Update JD

--- robots/jdsports.py
+++ robots/jdsports.py
@@ -1,21 +1,52 @@
+    # shoe_name,gender, email
 import tagui as t
 
-url = "https://www.jdsports.com.sg"
-closeLightbox_selector = ""
-search_selector = '//input[@name="q"]'
 
-search_query = "HOVR phantom" " men" "[enter]"
-search_query = "apex" " men" "[enter]"
+def get_shoe_jd(shoe_name, gender, email):
+    """
+    Get shoe details from jdsports.com.sg
+    :param shoe_name: name of the shoe to search for
+    :param gender: gender of the subscriber
+    :param email: email id of the subscriber
+    :return: details, list of shoe details.
+    """
+    details = []
+    t.init(visual_automation=True)
+    t.url('https://www.jdsports.com.sg/')
+    t.type('//input[@id = "srchInput"]', shoe_name + " shoes")
+    t.click('//input[@id ="srchButton"]')
+    t.wait(3)
 
-t.init()
-t.url(url)
-t.wait(3)
-if t.present():
-     
-     t.type(search_selector, search_query)
-count = t.count('//div[contains(@class," tile ")]//div[contains(@class,"name")]')
-if count > 3: count = 3
-for item in range(1, count + 1):
-     name = t.read(f'//div[contains(@class," tile ")][{item}]//div[contains(@class,"name")]')
-     cost = t.read(f'//div[contains(@class," tile ")][{item}]//div[contains(@class,"pricing")]')
-     print(item, name, cost)
+    if gender == 'male':
+        if t.read('(//a[@data-e2e="plp-filterMenu-catItem"]/span)[contains(.,"Men")]'):
+            t.click('(//a[@data-e2e="plp-filterMenu-catItem"]/span)[1]')
+            count = t.count('//ul[@id="productListMain"]//li[@class="productListItem "]')
+            t.wait(3)
+            for i in range(1, min(count, 4)):
+                price = t.read(f'(//span[@class="pri"])[{i}]')
+                name = t.read(f'(//span[@class="itemTitle"])[{i}]')
+                img = t.read(f'(//a[@class="itemImage"]/picture/img/@srcset)[{i}]')
+                details.append({"email": email, "name": name,
+                                "price": price, "img": img, "Company": "JDsports"})
+    elif gender == 'female':
+        if t.read('(//a[@data-e2e="plp-filterMenu-catItem"]/span)[contains(.,"Women")]'):
+            t.click('(//a[@data-e2e="plp-filterMenu-catItem"]/span)[.="Women"]')
+            count = t.count('//ul[@id="productListMain"]//li[@class="productListItem "]')
+            t.wait(3)
+            for i in range(1, min(count, 4)):
+                price = t.read(f'(//span[@class="pri"])[{i}]')
+                name = t.read(f'(//span[@class="itemTitle"])[{i}]')
+                img = t.read(f'(//a[@class="itemImage"]/picture/img/@srcset)[{i}]')
+                details.append({"email": email, "name": name, "price": price,
+                                "img": img, "Company": "JDsports"})
+    else:
+        count = t.count('//ul[@id="productListMain"]//li[@class="productListItem "]')
+        t.wait(3)
+        for i in range(1, min(count, 4)):
+            price = t.read(f'(//span[@class="pri"])[{i}]')
+            name = t.read(f'(//span[@class="itemTitle"])[{i}]')
+            img = t.read(f'(//a[@class="itemImage"]/picture/img/@srcset)[{i}]')
+            details.append({"email": email, "name": name, "price": price,
+                            "img": img, "Company": "JDsports"})
+    t.close()
+    return details
